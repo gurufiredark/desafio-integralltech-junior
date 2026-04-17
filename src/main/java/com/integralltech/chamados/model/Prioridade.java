573b5c5497b67342efd6e871ff4ba84572202cce
Gabriel Rodrigues <gabrielrodriguesguru@gmail.com>
2026-04-17 15:57:25 -0300

    chore: adicionar enums (Setor, Prioridade, Status)

---
+++ src/main/java/com/integralltech/chamados/model/Prioridade.java
@@ -0,0 +1,8 @@
+package com.integralltech.chamados.model;
+
+public enum Prioridade {
+    BAIXA,
+    MEDIA,
+    ALTA,
+    CRITICA
+}
---
+++ src/main/java/com/integralltech/chamados/model/Status.java
@@ -0,0 +1,8 @@
+package com.integralltech.chamados.model;
+
+public enum Status {
+    ABERTO,
+    EM_ANDAMENTO,
+    RESOLVIDO,
+    FECHADO
+}
